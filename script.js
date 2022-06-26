52731dbf44d25f95dfeb6a1a910c21f45c2dbef5
Samuel Palacios <samuelpl0888@gmail.com>
2022-06-26 04:09:20 -0400

    Create Regexs for validation

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const alertMsg = document.querySelector('#alertMessage');
 const form = document.querySelector('#myForm');
 const inputs = document.querySelectorAll('#myForm input');
+
+// Regex Formats
+
+const expressions = {
+  name: /^[a-zA-Z]+$/,
+  card: /^\d{13,16}$/,
+  cvc: /^\d{4}$/,
+  amount: /\d+/,
+  city: /[a-zA-Z]+/,
+  postalCode: /^\d{4,8}$/,
+};
